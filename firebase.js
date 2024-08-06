eb02c3beb978566e902103bf9aa823c024e72c2a
Oluwasegun Atolagbe <58482668+AtVict0r@users.noreply.github.com>
2024-08-06 09:17:49 -0400

    revert "remove upload artifact publish.yml"

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    apiKey: "AIzaSyCdJvy18zCXazX_wja3cR5P41HqLrRZSBI",
+    authDomain: "headstarter-pantry-track-2a4c1.firebaseapp.com",
+    projectId: "headstarter-pantry-track-2a4c1",
+    storageBucket: "headstarter-pantry-track-2a4c1.appspot.com",
+    messagingSenderId: "305735773592",
+    appId: "1:305735773592:web:b71c1e1b11cb67e1dacfed"
 };
 
 const app = initializeApp(firebaseConfig);
